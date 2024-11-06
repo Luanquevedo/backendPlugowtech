eb294635465f3c2ecce9a6da62ef55375c72a0e6
Luan Meira Quevedo <151397956+Luanquevedo@users.noreply.github.com>
2024-11-06 14:41:40 -0300

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,1 @@
-const express = require('express');
-const { register } = require('../controllers/userController');
 
-const router = express.Router();
-
-// Rota para regristros
-
-router.post('/register', register);
-
-module.exports = router;
