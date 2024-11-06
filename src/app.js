9c44da7600dc66ae9dbba08dc6ac126686c53e15
Luan Meira Quevedo <151397956+Luanquevedo@users.noreply.github.com>
2024-11-06 02:12:22 -0300

    alterado porta de comunicação com servidor para 8080

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 
 // Inicialização do servidor na porta da variavel
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
