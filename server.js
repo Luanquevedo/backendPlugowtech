cc3e55d58f7feeb1873bc1c9e66b23dc4c6baa1a
Luan Meira Quevedo <151397956+Luanquevedo@users.noreply.github.com>
2024-11-06 14:34:55 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = require('./src/app')
 
 require('dotenv').config();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
